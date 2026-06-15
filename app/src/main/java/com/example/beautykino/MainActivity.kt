da3f58e4ace790d27a6b226e7fdb2e29b9b05ed9
Darets <darets@gmail.com>
2026-06-15 21:23:35 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/beautykino/MainActivity.kt
+++ app/src/main/java/com/example/beautykino/MainActivity.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue // Добавленный фикс
+import androidx.compose.runtime.setValue // Добавленный фикс
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
